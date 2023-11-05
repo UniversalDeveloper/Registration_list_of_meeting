d322f4a145b3b9054c9905f4a9b096fc66d287a0
Anastasiia Lopata <lopata.ad@gmail.com>
2023-11-05 15:10:20 +0100

    created static client , created pac.json, started dev prouces of server

---
+++ server_app.js
@@ -0,0 +1,11 @@
+const express= require('express')//import expres in serv_app file
+const path= require('path')
+const app= express()//put  object express on app
+
+app.use(express.static(path.resolve(__dirname,'client')))//create static folder 
+
+app.get('*',(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'client','index.html'))
+}
+)
+app.listen(3000,()=> console.log('Server has been started on port 3000...'))//http://localhost:3000/
