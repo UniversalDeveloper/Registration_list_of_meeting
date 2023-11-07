f58f1b9a33c9af54ee11a69f9db4549de34e80a6
Anastasiia Lopata <lopata.ad@gmail.com>
2023-11-07 16:48:46 +0100

    filds on front.page will
    filds on front.page will
    created the object containing the entered data;
    after adding data the files on page will be cleaning, but the created object will be saved;

--- client/frontend.js
+++ client/frontend.js
@@ -1,11 +1,32 @@
 const { createApp, ref } = Vue
 
-  createApp({
+createApp({
+
+  el: '#app',
+  data(){
+return{
+  form: {
+    name: '',
+    value: ''
+  }
+}
+  },
+  methods:{
+    createContact(){
+      const{...contact}=this.form 
+     console.log(contact)
+    this.form.name= this.form.value=''
+    
+    }
+  }
+}).mount('#app')
+
+ /* createApp({
     setup() {
       const message = ref('Hello vue!')
       return {
         message
       }
     }
-  }).mount('#app')//connect with html
+  }).mount('#app')//connect with html*/
   // console.log(Vue);
